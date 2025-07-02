9906b3aaaaf68fcc4e64bc0056eea2e60c752842
safecs07 <botsafe002@gmail.com>
2025-07-02 20:23:37 +0700

    feat: domain order

---
+++ internal/domain/entity/order.go
@@ -0,0 +1,30 @@
+package entity
+
+// TODO: Add Validation
+type InputOrder struct {
+	No                int     `json:"no"`
+	PlatformProductId string  `json:"platformProductId"`
+	Qty               int     `json:"qty"`
+	UnitPrice         float64 `json:"unitPrice"`
+	TotalPrice        float64 `json:"totalPrice"`
+}
+
+type CleanedOrder struct {
+	No         int     `json:"no"`
+	ProductId  string  `json:"productId"`
+	MaterialId string  `json:"materialId,omitempty"`
+	ModelId    string  `json:"modelId,omitempty"`
+	Qty        int     `json:"qty"`
+	UnitPrice  float64 `json:"unitPrice"`
+	TotalPrice float64 `json:"totalPrice"`
+}
+
+type OrderBatch struct {
+	Orders []InputOrder
+}
+
+func NewOrderBatch(orders []InputOrder) *OrderBatch {
+	return &OrderBatch{
+		Orders: orders,
+	}
+}
