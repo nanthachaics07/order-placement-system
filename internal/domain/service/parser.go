27849ece759657a3bff1fe9b887aac5dd6ff05cc
safecs07 <botsafe002@gmail.com>
2025-07-03 22:05:37 +0700

    refact: move parser interface to domain service

---
+++ internal/domain/service/parser.go
@@ -0,0 +1,12 @@
+package service
+
+import "order-placement-system/internal/domain/entity"
+
+type ProductParser interface {
+	Parse(platformProductId string, originalQty int, totalPrice float64) ([]*entity.ParsedProduct, error)
+	CleanPrefix(productId string) string
+	ExtractQuantity(productId string) (cleanId string, quantity int, hasQuantity bool)
+	SplitBundle(productId string) []string
+	ParseProductCode(productId string) (materialId, modelId string, err error)
+	Validate(productId string) error
+}
