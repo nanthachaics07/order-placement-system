10b0c21dbf7c8cded714282d3f88b9608e187eef
safecs07 <botsafe002@gmail.com>
2025-07-04 16:32:18 +0700

    fix: update env default

--- env/env.go
+++ env/env.go
@@ -18,7 +18,7 @@ func LoadEnv() {
 	// GinMode = load_env.Require("GIN_MODE")
 	GinMode = load_env.Default("GIN_MODE", "release")
 	ServiceName = load_env.Default("SERVICE_NAME", "order-placement-system")
-	AppVersion = load_env.Default("APP_VERSION", "v1.0.5")
+	AppVersion = load_env.Default("APP_VERSION", "v1.0.4")
 	LogLevel = load_env.Default("LOG_LEVEL", "dev")
 	Port = load_env.Default("PORT", "8080")
 	ShutdownTimeout, _ = time.ParseDuration(load_env.Default("SHUTDOWN_TIMEOUT", "5s"))
